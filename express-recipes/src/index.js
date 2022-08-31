129c303fa85c39981fc266a48e07b6bfdb97b4b2
Clyne Fuentes <clippefuentes@gmail.com>
2022-08-30 22:11:24 -0600

    add authentication

--- express-recipes/src/index.js
+++ express-recipes/src/index.js
@@ -3,7 +3,9 @@ const path = require('path');
 const cors = require("cors");
 
 const recipesRouter = require('./routers/recipes');
+const usersRouter = require('./routers/users');
 const { handleError } = require('./utils/error');
+const auth = require('./middleware/auth.js');
 
 const app = express();
 
@@ -19,11 +21,14 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(auth.initialize());
+
 app.get("/", (req, res) => {
   res.redirect("/api/v1/recipes");
 });
 
 app.use('/api/v1/recipes', recipesRouter);
+app.use("/api/v1/users", usersRouter);
 
 app.use(handleError);
 
