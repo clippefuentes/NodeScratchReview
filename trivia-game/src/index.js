c60ba41cb82dc7c2de3998d6e7f44d0ac5b36318
Clyne Fuentes <clippefuentes@gmail.com>
2022-08-31 13:18:29 -0600

    add chat functionality and refractor socket

--- trivia-game/src/index.js
+++ trivia-game/src/index.js
@@ -3,6 +3,15 @@ const http = require('http');
 const path = require('path');
 const socketio = require('socket.io');
 
+const formatMessage = require("./utils/formatMessage.js");
+
+const {
+  addPlayer,
+  getAllPlayers,
+  getPlayer,
+  removePlayer,
+} = require("./utils/players.js");
+
 const port = process.env.PORT || 8080;
 
 const app = express();
@@ -12,9 +21,76 @@ const io = socketio(server); // connect Socket.IO to the HTTP server
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => { // listen for new connections to Socket.IO
+io.on('connection', socket => { // listen for new connections to Socket.IO
   console.log('A new player just connected');
+  socket.on('join', ({ playerName, room }, callback) => {
+    const { error, newPlayer } = addPlayer({ id: socket.id, playerName, room });
+
+    if (error) return callback(error.message);
+    callback(); // The callback can be called without data.
+
+    socket.join(newPlayer.room);
+
+    socket.emit('message', formatMessage('Admin', 'Welcome!'));
+
+    socket.broadcast
+      .to(newPlayer.room)
+      .emit(
+        'message',
+        formatMessage('Admin', `${newPlayer.playerName} has joined the game!`)
+      );
+
+      socket.on("disconnect", () => {
+        console.log("A player disconnected.");
+      
+        const disconnectedPlayer = removePlayer(socket.id);
+      
+        if (disconnectedPlayer) {
+          const { playerName, room } = disconnectedPlayer;
+          io.in(room).emit(
+            "message",
+            formatMessage("Admin", `${playerName} has left!`)
+          );
+      
+          io.in(room).emit("room", {
+            room,
+            players: getAllPlayers(room),
+          });
+        }
+      });
+
+    io.in(newPlayer.room).emit('room', {
+      room: newPlayer.room,
+      players: getAllPlayers(newPlayer.room),
+    });
+
+    socket.on("sendMessage", (message, callback) => {
+      const { error, player } = getPlayer(socket.id);
+    
+      if (error) return callback(error.message);
+    
+      if (player) {
+        io.to(player.room).emit(
+          "message",
+          formatMessage(player.playerName, message)
+        );
+        callback(); // invoke the callback to trigger event acknowledgment
+      }
+    });
+
+  });
 })
+
+io.on('join', ({ playerName, room }, callback) => {
+  const { error, newPlayer } = addPlayer({ id: socket.id, playerName, room });
+
+  if (error) return callback(error.message);
+  callback(); // The callback can be called without data.
+
+  socket.join(newPlayer.room);
+
+  socket.emit('message', formatMessage('Admin', 'Welcome!'));
+});
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port}.`);
